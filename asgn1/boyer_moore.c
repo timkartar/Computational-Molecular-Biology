912d5e94c5e5d25fe4862686526c3e579ef14503
timkartar <timkartar7879@gmail.com>
2020-02-03 15:31:36 -0800

    Z algorithm as function in C

--- asgn1/boyer_moore.c
+++ asgn1/boyer_moore.c
@@ -11,13 +11,41 @@ int match (const char* s, size_t q, size_t i){
 }
 
 size_t* comp_z(const char* p){
-    int j = strlen(p);
-    size_t* i = (size_t*)malloc(sizeof(size_t));
-    *i = j;
-    return i;
+    //int j = strlen(p);
+    //size_t* i = (size_t*)malloc(sizeof(size_t));
+    //*i = j;
+    //return i;
+    size_t* Z = (size_t*)malloc(sizeof(size_t)*strlen(p));
+    Z[0]=0;
+    char* the_case = "0";
+    size_t l=0, r=0;
+    for(size_t k=1; k< strlen(p); ++k){
+        if(k >= r){  //case 1: full comparison
+            the_case = "1";
+            Z[k] = match(p, 0, k);
+            r = k + Z[k];
+            l = k;
+        }
+        else { //case 2: inside a Z-box
+            const size_t k_prime = k - 1;
+            const size_t beta_len = r - k;
+            if (Z[k_prime] < beta_len){ // Case 2a: stay inside z-box
+                the_case = "2a";
+                Z[k] = Z[k_prime];
+            }
+            else{ //Case 2b: need to match outside the Z-box
+                the_case = "2b";
+                Z[k] = match(p,beta_len,r);
+                r = k + Z[k];
+                l = k;
+            }
+        }
+        printf("%zu\t%zu\t%zu\t%zu\t%s\n",k+1,l+1,r,Z[k],the_case);
+    }
+    return Z;
 }
 int main(){
-	size_t* a = comp_z("abcd");
-	printf("%d\n",(int)*a);
+	comp_z("AAAAACAGTTACCCAATGACA");
+    return EXIT_SUCCESS;
 }   
 
