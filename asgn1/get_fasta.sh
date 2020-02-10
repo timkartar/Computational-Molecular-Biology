693e1dcb957dac686d043800d1b5e00fa83f5ab5
timkartar <timkartar7879@gmail.com>
2020-02-09 16:29:08 -0800

    added human genome GRch38 download instructions

--- asgn1/get_fasta.sh
+++ asgn1/get_fasta.sh
@@ -5,3 +5,12 @@ wget ftp://ftp.ensembl.org/pub/release-99/fasta/oryctolagus_cuniculus/dna/Orycto
 gunzip Oryctolagus_cuniculus.OryCun2.0.dna.*
 cat Oryctolagus_cuniculus.OryCun2.0.dna.* > rabbit.fasta
 cd ../
+
+mkdir human
+cd human
+wget ftp://ftp.ensembl.org/pub/release-99/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.*
+gunzip Homo_sapiens.GRCh38.dna.*
+cat Homo_sapiens.GRCh38.dna.* > human.fasta
+cd ../
+
+
